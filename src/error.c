04fdd43a0485fa281f5f82543c596f1928b41760
42yasuke <j.ralph1998@gmail.com>
2023-04-10 04:05:10 +0200

    julia

--- src/error.c
+++ src/error.c
@@ -6,16 +6,23 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 16:56:02 by jose              #+#    #+#             */
-/*   Updated: 2023/03/17 15:41:51 by jose             ###   ########.fr       */
+/*   Updated: 2023/04/10 03:47:30 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+static void	ft_usage(void)
+{
+	ft_printf("******** tutoriel ********\n");
+	ft_printf("mandelbrot --> ./fractol m\n");
+	ft_printf("julia --> ./fractol j x y\n");
+}
+
 void	ft_error(int err, char *msg_err)
 {
 	if (err == BAD_PARAMETERS)
-		ft_printf("Error : %s\n", msg_err);
+		(ft_printf("Error : %s\n", msg_err), ft_usage());
 	else if (err == MLX_INIT_FAILED)
 		ft_printf("Error : %s\n", msg_err);
 	else if (err == MLX_WIN_FAILED)
--- src/main.c
+++ src/main.c
@@ -6,19 +6,14 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 16:50:52 by jose              #+#    #+#             */
-/*   Updated: 2023/03/26 17:56:01 by jose             ###   ########.fr       */
+/*   Updated: 2023/04/10 03:58:03 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	main(int ac, char **av)
+static int	suite_main (t_win *win)
 {
-	t_win	*win;
-
-	if (ac != 2)
-		ft_error(BAD_PARAMETERS, "bad parameters");
-	win = ft_initial_window(av[1]);
 	mlx_loop_hook(win->mlx, &ft_draw_fractal, win);
 	mlx_hook(win->mlx_win, KeyPress, KeyPressMask, &ft_event_manager, win);
 	mlx_hook(win->mlx_win, ClientMessage, StructureNotifyMask, \
@@ -28,4 +23,14 @@ int	main(int ac, char **av)
 	mlx_destroy_display(win->mlx);
 	(free(win->mlx), free(win));
 	return (EXIT_SUCCESS);
+}
+
+int	main(int ac, char **av)
+{
+	t_win	*win;
+
+	if (ac == 2 && )
+		ft_error(BAD_PARAMETERS, "bad parameters");
+	win = ft_initial_window(av[1]);
+	return (suite_main(win));
 }
